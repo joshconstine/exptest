54e4382e289f178b3e38d38d65e7fa424941e0fb
joshua constine <joshua.constine97@gmail.com>
2023-11-05 14:27:20 -0800

    showing list of recipes

--- App.tsx
+++ App.tsx
@@ -1,63 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image } from "react-native";
+type Recipe = {
+  Recipe_id: number;
+  Name: string;
+  Description: string;
+  Photos: string[];
+  Tags: Tag[];
+  TagString: string;
+  Ingredients: Ingredient[];
+};
+
+type Tag = {
+  Tag_id: number;
+  Name: string;
+};
+
+type Ingredient = {
+  Ingredient_id: number;
+  Name: string;
+  Quantity: number;
+  Quantity_type: string;
+  Quantity_type_id: number;
+};
 
 export default function App() {
-
-  const [recipes, setRecipes] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getRecipes = async () => {
-    try{
-      
-      const response = await fetch('http://172.21.0.3:8080/api/mobile/v1/recipes', {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-        },
-
-      }); // Replace with your Docker container's IP or hostname if needed
+    try {
+      const response = await fetch(
+        "http://172.21.0.3:8080/api/mobile/v1/recipes",
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      ); // Replace with your Docker container's IP or hostname if needed
       const data = await response.json();
-      console.log(data);
       setRecipes(data);
       setIsLoading(false);
-
     } catch (error) {
-      console.error('Error  recipes. Status:', error);
+      console.error("Error  recipes. Status:", error);
       setIsLoading(false);
     }
-
   };
 
   useEffect(() => {
     getRecipes();
-  }
-  , []);
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Listify</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.title}>Listify</Text>
+        <ActivityIndicator animating={isLoading} size="large" />
 
-      <StatusBar style="auto" />
-      {isLoading ? <Text>Loading...</Text> : (
-        <View>
-          {recipes.length === 0 && <Text>No recipes found</Text>}
-          {recipes.map((recipe: any) => (
-            <Text key={recipe.Id}>{recipe.Name}</Text>
-          ))}
-    </View>
-      )}
+        {!isLoading && recipes.length === 0 && <Text>No recipes found</Text>}
 
-    </View>
-          
+        {recipes.map((recipe: Recipe) => (
+          <View key={recipe.Recipe_id} style={styles.recipeContainer}>
+            {recipe.Photos.length > 0 && (
+              <Image
+                source={{ uri: recipe.Photos[0] }}
+                style={styles.recipeImage}
+              />
+            )}
+            <Text style={styles.recipeName}>{recipe.Name}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#fff",
+  },
+  safeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  recipeContainer: {
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  recipeImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+  },
+  recipeName: {
+    fontSize: 18,
+    marginTop: 10,
   },
 });
